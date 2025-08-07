46f27407a50f130851f83102abf0263e81b1e4fb
Teejuta Sriwaranon <teejuta.sriwaranon@gmail.com>
2025-08-07 10:29:10 +0700

    test: success testcase

--- tests/useCounter.spec.ts
+++ tests/useCounter.spec.ts
@@ -7,6 +7,6 @@ describe("useCounter", () => {
       result.current.setVal(5);
       result.current.increment();
     });
-    expect(result.current.count).toBe(5);
+    expect(result.current.count).toBe(1);
   });
 });
